b69a4658a50e8514bd8b0aba3dd8d7207bcdf126
Ajulmaharjan <ajul@codepros.com.np>
2022-08-13 00:37:43 +0545

    rework: home
    seperated html files

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+
+
+class HomeView(LoginRequiredMixin, View):
+    def get(self, request):
+        return HttpResponse("Here's the text of the web page.")
