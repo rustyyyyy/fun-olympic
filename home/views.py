f42a7e8d81dfb424b2a519834f8d7cd83ae727ab
Ajulmaharjan <ajul@codepros.com.np>
2022-08-30 21:45:39 +0545

    add: pagination and video layout

--- home/views.py
+++ home/views.py
@@ -5,12 +5,20 @@ from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from .models import Video
+from django.core.paginator import Paginator
 
 
-class HomeView(LoginRequiredMixin, View):
+# class HomeView(LoginRequiredMixin, View):
+class HomeView(View):
     def get(self, request):
         videos = Video.objects.all()
+
+        paginator = Paginator(videos, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         context = {
-            'videos': videos
+            'page_obj': page_obj
         }
         return render(request, 'home/video/index.html', context)
